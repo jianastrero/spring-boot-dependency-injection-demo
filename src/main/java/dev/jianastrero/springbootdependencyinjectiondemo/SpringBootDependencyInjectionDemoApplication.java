a67ab52c7037a821503c5bf06350c0e814ed347a
Jian James Astrero <jianjamesastrero@gmail.com>
2023-09-30 01:36:02 +0800

    Shift setting of custom message in dependency injection demo
    Refactored the code to move the setting of custom message from injectMe to injectMe2 in SpringBootDependencyInjectionDemoApplication. This is in line with demonstrating the singleton behaviour of beans in Spring, i.e., changing the property of one should reflect in others. The change also helps in ensuring consistency of displayed messages across all InjectMe beans.

--- src/main/java/dev/jianastrero/springbootdependencyinjectiondemo/SpringBootDependencyInjectionDemoApplication.java
+++ src/main/java/dev/jianastrero/springbootdependencyinjectiondemo/SpringBootDependencyInjectionDemoApplication.java
@@ -21,7 +21,6 @@ public class SpringBootDependencyInjectionDemoApplication {
 
         // Get the bean for InjectMe
         InjectMe injectMe = context.getBean(InjectMe.class);
-        injectMe.setMessage("Custom text for injectMe");
         System.out.println("> injectMe: " + injectMe);
         injectMe.printMessage();
 
@@ -30,11 +29,21 @@ public class SpringBootDependencyInjectionDemoApplication {
         // Because it is a singleton
         // It is only created once
         // And it is reused
-        // We changed the message for injectMe
-        // But it is still the same for injectMe2
         InjectMe injectMe2 = context.getBean(InjectMe.class);
+        injectMe2.setMessage("Custom text for injectMe");
         System.out.println("> injectMe2: " + injectMe2);
         injectMe2.printMessage();
+
+        // Get the bean for InjectMe
+        // This should be the same as the first one
+        // Because it is a singleton
+        // It is only created once
+        // And it is reused
+        // But since we changed the message for injectMe2
+        // The message for injectMe3 is also changed
+        InjectMe injectMe3 = context.getBean(InjectMe.class);
+        System.out.println("> injectMe3: " + injectMe3);
+        injectMe3.printMessage();
 
         // Get the bean for InjectToMe
         // This should be the same as the first one
