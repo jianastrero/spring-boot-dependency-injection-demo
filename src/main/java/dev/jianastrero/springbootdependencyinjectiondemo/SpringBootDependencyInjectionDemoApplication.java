4f2e738d9b677befea3bf1c234650393c0d9baa1
Jian James Astrero <jianjamesastrero@gmail.com>
2023-09-30 00:53:11 +0800

    Spring Boot Dependency Injection Demo

---
+++ src/main/java/dev/jianastrero/springbootdependencyinjectiondemo/SpringBootDependencyInjectionDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.jianastrero.springbootdependencyinjectiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDependencyInjectionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDependencyInjectionDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jianastrero/springbootdependencyinjectiondemo/SpringBootDependencyInjectionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jianastrero.springbootdependencyinjectiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDependencyInjectionDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
